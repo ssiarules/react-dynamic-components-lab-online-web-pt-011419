d6e496ccb11ae2fc316d702f21e037107b3ccd57
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-05 20:44:59 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -21,7 +21,7 @@ export default class BlogPost extends Component {
         You only grew accustomed to the salt<br/>
         I was born in it<br/>
 
-        <Comment commentText={"Tell my wifi love her"} />
+        <Comment commentText={comments[0]} />
         <Comment commentText={comments[1]} />
         <Comment commentText={comments[2]} />
       </div>
