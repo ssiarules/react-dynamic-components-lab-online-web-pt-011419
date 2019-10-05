caf22fbf15f61f48acdeef577b6c70bab134f53c
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-05 20:18:56 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
 // add Comment component here
+import React, { Component } from 'react'
+
+export class Comment extends Component {
+  render() {
+    return (
+      <div>
+          {this.props.commentText}
+      </div>
+    )
+  }
+}
